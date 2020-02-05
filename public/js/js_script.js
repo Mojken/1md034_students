d47003116bce92179dd900b9ac11ca30f8689241
Mojken <mikaelgillis@gmail.com>
2020-02-05 22:20:08 +0100

    Pure js isn't always awful. Just mostly.

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -50,17 +50,17 @@ let e = new menuItem(
 );
 
 function burger(burger) {
-  ret = "";
+  ret = "<div class='menuborgar'>";
   ret += "<h2>" + burger.name + "</h2>";
-  ret += '<div class="box"><img src="' + burger.img + '" width="300"></div>';
+  ret += '<div class="box image-div"><img src="' + burger.img + '" width="300"></div>';
   ret += "<div class='box'>"
   ret += "<dt>Calories</dt><dd>" + burger.calories + "</dd>";
   ret += "<dt>Content</dt><dd>" + burger.contents.join(', ').replace(/^\w/, c => c.toUpperCase()) + "</dd>";
   ret += "<dt>Allergy</dt><dd>" + burger.allergy.join(', ').replace(/^\w/, c => c.toUpperCase()) + "</dd>";
-  ret += "</div>"
+  ret += "</div></div>"
   return ret;
 }
 
-let burgers = [a, b, c, d, d]; 
+let burgers = [a, b, c, d, e]; 
 
 document.getElementById("borgar-wrapper").innerHTML += burgers.map(x => burger(x)).join('');
